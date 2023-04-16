db431e0b97640b14f3552acd840b4ab3472dd7a7
CL4UD3PT <cifpinho@gmail.com>
2023-04-16 19:43:38 +0000

    updated navbar with title and favorites dropdown placebo

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -2,8 +2,18 @@ import React from "react";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-
+		<nav className="navbar navbar-light bg-light mb-3 p-2">
+			<h1 className="fs-4">Rick & Morty BlogWars</h1>
+			<div className="btn-group">
+				<button type="button" className="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+					Favorites
+				</button>
+				<ul className="dropdown-menu dropdown-menu-end">
+					<li className="dropdown-item" >Action</li>
+					<li className="dropdown-item" >Another action</li>
+					<li className="dropdown-item" >Something else here</li>
+				</ul>
+			</div>
 		</nav>
 	);
 };
