0fe2700883784c4fc70445c436055a9f7d7e16bd
CL4UD3PT <98550012+CL4UD3PT@users.noreply.github.com>
2023-04-29 17:17:40 +0000

    now can select a suggestion in search bar by clicking on it

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -6,13 +6,20 @@ import schwifty from "../../img/get-schwifty.png";
 export const Navbar = () => {
 	const {store, actions} = useContext(Context);
 	const [search, setSearch] = useState("")
-	const [searchSuggestionsOn, setSearchSuggestionsOn] = useState(true);
 	const [suggestionsData, setSuggestionsData] = useState([]);
+	const [suggestionClicked, setSuggestionClicked] = useState(false);
+	const [searchSuggestionsOn, setSearchSuggestionsOn] = useState(true);
 
 	const handleSearchString = (searchString) => {
 		let find = store[store.schema].filter((char) => char.name.toLowerCase().includes(searchString.toLowerCase()));
 		searchString.length == 0 ? setSuggestionsData('') : setSuggestionsData(find);
 		actions.setDataFiltered(find);
+	}
+
+	const handleClickedSuggestion = (suggestion) => {
+		setSuggestionClicked(true);
+		setSearch(suggestion);
+		handleSearchString(suggestion);
 	}
 
 	return (
@@ -53,19 +60,19 @@ export const Navbar = () => {
 
 					{/* AUTO-SEARCH */}
 					<div className="input-group auto-search-container col-12 col-lg-3 mb-3 mb-lg-0">
-						<input type="text" className="form-control" onChange={(e)=>{setSearch(e.target.value)}} onKeyUp={(e)=>{handleSearchString(search)}} value={search} placeholder="Search..." aria-label="Search"></input>
+						<input type="text" className="form-control" onChange={(e)=>{setSearch(e.target.value)}} onKeyUp={(e)=>{suggestionClicked?setSuggestionClicked(false):null;handleSearchString(search)}} value={search} placeholder="Search..." aria-label="Search"></input>
 						<button type="button" className="btn btn-outline-secondary dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" data-bs-auto-close="outside" aria-expanded="false">
 							<span className="visually-hidden">Toggle Dropdown</span>
 						</button>
 						<ul className="dropdown-menu dropdown-menu-end">
 							<li className="dropdown-item">
-								<input className="form-check-input me-2" onClick={()=>setSearchSuggestionsOn(!searchSuggestionsOn)} type="checkbox" value="" id="flexCheckDefault" checked={searchSuggestionsOn}/>
+								<input className="form-check-input me-2" onChange={()=>setSearchSuggestionsOn(!searchSuggestionsOn)} type="checkbox" value="" id="flexCheckDefault" checked={searchSuggestionsOn}/>
 								<label className="form-check-label" htmlFor="flexCheckDefault">Search suggestions on</label>
 							</li>
 						</ul>
-						{suggestionsData.length > 0 && searchSuggestionsOn
+						{suggestionsData.length > 0 && searchSuggestionsOn && !suggestionClicked
 							?<div className="dropdown-auto-search rounded shadow">
-								{suggestionsData.map((item) => <div key={item.id} className="dropdown-item p-1">{item.name}</div>)}
+								{suggestionsData.map((item) => <div key={item.id} className="dropdown-item p-1" onClick={()=>handleClickedSuggestion(item.name)}>{item.name}</div>)}
 							</div>
 							:null
 						}
